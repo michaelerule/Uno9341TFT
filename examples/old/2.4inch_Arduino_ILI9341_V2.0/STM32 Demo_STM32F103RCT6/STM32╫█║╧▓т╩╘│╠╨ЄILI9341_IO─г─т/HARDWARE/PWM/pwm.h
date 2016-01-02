4dbb8c9a4cb26951d8f0399a82837399f92d1b53
Michael Rule <MRule7404@gmail.com>
2016-01-02 17:08:45 -0500

    removed obsolete examples

--- examples/old/2.4inch_Arduino_ILI9341_V2.0/STM32 Demo_STM32F103RCT6/STM32╫█║╧▓т╩╘│╠╨ЄILI9341_IO─г─т/HARDWARE/PWM/pwm.h
+++
@@ -1,9 +0,0 @@
-#ifndef __PWM_H
-#define __PWM_H
-#include "sys.h"
-
-
-
-void PWM_Init(u16 arr,u16 psc);
-
-#endif
--- examples/old/Uno_TFT_3D_normal_test/sort_triangles.h
+++
@@ -1,6 +0,0 @@
-/* Routine for sorting a list of triangles */
-void sort_triangles(const uint8_t *triangles, uint8_t ntriangles, uint8_t *permutation) {
-
-
-}
-
